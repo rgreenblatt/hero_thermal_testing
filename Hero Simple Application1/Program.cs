47f97c3c16256bebcdf0120bddeaa097cb5b9614
rgreenblatt <hobo.ryang@gmail.com>
2018-04-17 15:51:30 -0400

    misc changes - great commit msg

--- Hero Simple Application1/Program.cs
+++ Hero Simple Application1/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
+using CTRE;
 
 namespace Hero_Simple_Application1
 {
@@ -9,19 +10,35 @@ namespace Hero_Simple_Application1
     {
         public static void Main()
         {
+            CTRE.TalonSrx talon = new CTRE.TalonSrx(0);
+            talon.SetControlMode(TalonSrx.ControlMode.kVoltage);
+            talon.SetFeedbackDevice(TalonSrx.FeedbackDevice.CtreMagEncoder_Absolute);
+            talon.SetSensorDirection(false);
+            talon.SetVoltageRampRate(0.0f);
+
             /* simple counter to print and watch using the debugger */
             int counter = 0;
             /* loop forever */
             while (true)
             {
                 /* print the three analog inputs as three columns */
-                Debug.Print("Counter Value: " + counter);
+                //Debug.Print("Counter Value: " + counter);
+                CTRE.Watchdog.Feed();
 
+                talon.Set(0.0f); //low ish voltage
                 /* increment counter */
                 ++counter; /* try to land a breakpoint here and hover over 'counter' to see it's current value.  Or add it to the Watch Tab */
 
                 /* wait a bit */
-                System.Threading.Thread.Sleep(100);
+                //using(System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Public\TestFolder\data.srv"))
+                //{
+                double seconds = DateTime.Now.Minute * 60 + DateTime.Now.Second + DateTime.Now.Millisecond / 1000.0;
+                //CTRE.TalonSrx.VelocityMeasurementPeriod.Period_100Ms period;
+                Debug.Print(seconds.ToString() + "," + talon.GetPosition().ToString() + "," + talon.GetSpeed().ToString() + "," + talon.GetOutputVoltage().ToString() + "," + talon.GetOutputCurrent().ToString());
+                //talon.SetVelocityMeasurementPeriod(CTRE.TalonSrx.VelocityMeasurementPeriod.Period_10Ms);
+                //  file.WriteLine(seconds.ToString() + "," + talon.GetPosition().ToString() + "," + talon.GetSpeed().ToString());
+                //}
+                System.Threading.Thread.Sleep(10);
             }
         }
     }
